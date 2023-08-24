2c0cb88f8a9f3b9a6222d9e894b92d72e8316a12
racharlamythily <racharlamythily4@gmail.com>
2023-08-24 07:01:39 -0500

    first commit

--- app/views.py
+++ app/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
-
+from app.models import *
 # Create your views here.
+
+def display_topics(request):
+    QSTO=Topic.objects.all()
+    d={'QSTO':QSTO}
+    return render(request,'display_topics.html',d)
+
+
+def display_webpages(request):
+    QSWO=Webpage.objects.all()
+    d={'QSWO':QSWO}
+    return render(request,'display_webpages.html',d)
+
+def display_accessrecord(request):
+    QSARO=Accessrecords.objects.all()
+    d={'QSARO':QSARO}
+    return render(request,'display_accessrecord.html',d)
